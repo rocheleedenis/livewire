c9c43f3022d8b7cf8f3dd4da13479d33283c3fad
Rochele Edenís <rochele.edenis@gmail.com>
2021-11-15 22:42:19 -0300

    Setting up the form

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class Register extends Component
 {
+    public $name = '';
+    public $email = '';
+    public $password = '';
+    public $passwordConfirmation = '';
+
     public function render()
     {
         return view('livewire.auth.register');
+    }
+
+    public function register()
+    {
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
     }
 }
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,20 @@
-<div>
-    Register
-</div>
+<form wire:submit.prevent="register">
+    <div>
+        <label for="name">name</label>
+        <input wire:model="name" type="text" name="name" id="name">
+    </div>
+    <div>
+        <label for="email">email</label>
+        <input wire:model="email" type="text" name="email" id="email">
+    </div>
+    <div>
+        <label for="password">password</label>
+        <input wire:model="password" type="password" name="password" id="password">
+    </div>
+    <div>
+        <label for="passwordConfirmation">passwordConfirmation</label>
+        <input wire:model="password" type="password" name="passwordConfirmation" id="passwordConfirmation">
+    </div>
+
+    <button type="submit">Submit</button>
+</form>
